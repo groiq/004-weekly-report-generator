715312d6a00fc67967db662a83181190377d5e84
groiq <35578027+groiq@users.noreply.github.com>
2018-07-02 12:42:41 +0200

    wrote an errlog routine, added gitignore

--- report_generator_4.0.py
+++ report_generator_4.0.py
@@ -3,6 +3,9 @@
 # Imports
 # -------
 
+output = list()
+errlog = list()
+
 from argparse import ArgumentParser
 
 # Parameters
@@ -12,12 +15,29 @@ parser = ArgumentParser(fromfile_prefix_chars='@',
                         description="read a timelog and generate a report")
 parser.add_argument("-i", "--interactive", action="store_true", 
                     help="interactive mode")
+parser.add_argument("-m", "--mail", action="store_true",
+                    help="send report via email")
                     
 args = parser.parse_args()
 
 
 
 
+# Write output
+# ------------
+
+if errlog:
+    outfile = open("errlog.txt", "w", encoding="utf-8")
+    for entry in errlog:
+        try:
+            outfile.write(entry)
+        except:
+            outfile.write(format(entry))
+        outfile.write("\n")
+    outfile.close()
+    
+# The actual outfile will be formatted using the dates evaluated.
+# All this later.
 
 # Final readln
 # ------------
