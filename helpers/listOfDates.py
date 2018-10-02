687f54b7c23537b0e3b36fe63a6a1bdff64e63d7
groiq <35578027+groiq@users.noreply.github.com>
2018-10-02 13:26:14 +0200

    autogenerate missing reports - done

--- helpers/listOfDates.py
+++ helpers/listOfDates.py
@@ -6,14 +6,16 @@
 import datetime
 
 start_date = datetime.date(2018,6,11)
-end_date = datetime.date(2018,9,3)
+end_date = datetime.date(2018,9,23)
 
 outfile = open("reportLines.txt","w",encoding="utf-8")
 
 d = start_date
 delta = datetime.timedelta(days=1)
 while d <= end_date:
+    outfile.write("bbb ")
     outfile.write(d.strftime("%Y-%m-%d"))
+    outfile.write(" :-/ unknown")
     outfile.write("\n")
     d += delta
     
