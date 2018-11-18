1288bdc038a36cd0090dd713aa2869128ba98c7f
groiq <35578027+groiq@users.noreply.github.com>
2018-11-18 15:36:58 +0100

    set up date variables

--- compareWeeks.py
+++ compareWeeks.py
@@ -9,3 +9,14 @@
 from pprint import pprint
 from datetime import date,datetime,timedelta
 
+# Variables
+# ---------
+
+firstMonday = datetime.date(2017,8,28)
+firstSunday = datetime.date(2017,9,3)
+
+tag = datetime.timedelta(1,0,0)
+
+curMonday = firstMonday
+curSunday = firstSunday
+
