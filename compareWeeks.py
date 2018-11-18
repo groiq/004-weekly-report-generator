916b67d955871a4aecf0e4a9586b8bc29fed3169
groiq <35578027+groiq@users.noreply.github.com>
2018-11-18 12:40:59 +0100

    setting up some imports

--- compareWeeks.py
+++ compareWeeks.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+# This will retrieve summary reports from the toggl API for consecutive weeks
+# and store the logged times by project for further comparisons.
+
+# Imports
+# -------
+
+from pprint import pprint
+from datetime import date,datetime,timedelta
+
