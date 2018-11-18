94faad56fcaeadf5ad90d0a153c191451fc1db10
groiq <35578027+groiq@users.noreply.github.com>
2018-11-18 23:07:55 +0100

    first attempt at json import

--- refactorJSON.py
+++ refactorJSON.py
@@ -5,3 +5,20 @@
 #  outer layer with an entry "summary" containing the last entry and an entry
 #  "data" containing the original array minus the summary.
 
+import json
+from pprint import pprint
+
+filePath = "./data/compareWeeksJson.txt"
+
+# read data from file
+# -------------------
+#json = json.load(open(filePath,"r",encoding="utf-8"))
+intext = open(filePath,"r",encoding="utf-8").read()
+
+#incoming = json.loads(open('./data/compareWeeksJson.txt').read())
+#pprint(intext)
+parsed = json.loads(intext)
+#print(incoming[-1])
+
+# Issue is that json parser can't handle single quotes. Also, cannot handle
+#  datetime because not a standard variable type.
