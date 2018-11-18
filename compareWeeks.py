d4efc147e1fd76544fa213e6c856a81c5b37fb62
groiq <35578027+groiq@users.noreply.github.com>
2018-11-18 17:13:17 +0100

    set up api call

--- compareWeeks.py
+++ compareWeeks.py
@@ -8,14 +8,23 @@
 
 from pprint import pprint
 from datetime import date,datetime,timedelta
+from TogglPy import Toggl # https://github.com/matthewdowney/TogglPy
+toggl = Toggl()
+from time import sleep
+
+
 
 # Variables
 # ---------
 
 firstMonday = date(2017,8,28)
 firstSunday = date(2017,9,3)
+# dates for API tests
+firstMonday = date(2018,11,5)
+firstSunday = date(2018,11,11)
 
-tag = timedelta(1,0,0)
+
+dayDelta = timedelta(1,0,0)
 
 curMonday = firstMonday
 curSunday = firstSunday
@@ -23,6 +32,22 @@ curSunday = firstSunday
 testOutput = [{'mon': '12766-03-1', 'sun': '12766-03-7', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-8', 'sun': '12766-03-14', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-15', 'sun': '12766-03-21', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-22', 'sun': '12766-03-28', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}]
 
 datalog = []
+projectList = []
+
+idPath = "data/id.txt"
+idVals = {}
+idFile = open(idPath, "r", encoding="utf-8")
+idPath = idFile.readline().strip()
+idFile.close()
+idFile = open(idPath, "r", encoding="utf-8")
+for line in idFile:
+    line = line.strip().split("::")
+    idKey = line[0]
+    idVal = line[1]
+    idVals[idKey] = idVal
+testOutput.append(idVals)
+toggl.setAPIKey(idVals["token"])
+
 
 # loop through consecutive weeks
 # ------------------------------
@@ -36,14 +61,28 @@ while curSunday <= date.today():
     curWeek["Sun"] = curSunday
     curWeek["times"] = {}
 
+    requestParams = {
+        "workspace_id": idVals["id"],
+        "user_agent": idVals["uagent"],
+        "since": curMonday,
+        "until": curSunday,
+    }
+
+    response = toggl.request("https://toggl.com/reports/api/v2/details",
+                parameters=requestParams)
+    print("retrieved {} - {}. Sleeping...".format(curMonday,curSunday))
+    testOutput.append(response)
+    testOutput.append("="*50)
+    sleep(2)
+
 
 
     datalog.append(curWeek)
 
     # move to next week
     # -----------------
-    curMonday += tag * 7
-    curSunday += tag * 7
+    curMonday += dayDelta * 7
+    curSunday += dayDelta * 7
 
     
 
@@ -58,6 +97,6 @@ while curSunday <= date.today():
 
 with open("./data/compareWeeksJson.txt","w",encoding="utf-8") as outfile:
     pprint(testOutput,outfile)
-    
+    outfile.write("\n{}\n".format("="*60))
     pprint(datalog,outfile)
 
