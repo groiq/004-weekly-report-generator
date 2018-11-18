d8278cf404a7d6d2cb03b301b3c7bcc6a8362319
groiq <35578027+groiq@users.noreply.github.com>
2018-11-18 15:58:42 +0100

    set up output file

--- compareWeeks.py
+++ compareWeeks.py
@@ -12,11 +12,26 @@ from datetime import date,datetime,timedelta
 # Variables
 # ---------
 
-firstMonday = datetime.date(2017,8,28)
-firstSunday = datetime.date(2017,9,3)
+firstMonday = date(2017,8,28)
+firstSunday = date(2017,9,3)
 
-tag = datetime.timedelta(1,0,0)
+tag = timedelta(1,0,0)
 
 curMonday = firstMonday
 curSunday = firstSunday
+
+testOutput = [{'mon': '12766-03-1', 'sun': '12766-03-7', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-8', 'sun': '12766-03-14', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-15', 'sun': '12766-03-21', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}, {'mon': '12766-03-22', 'sun': '12766-03-28', 'times': {'coding': '15:00:00', 'writing': '15:00:00', 'reading': '15:00:00', 'gaming': '15:00:00'}}]
+
+datalog = []
+
+
+
+
+
+
+
+with open("./data/compareWeeksJson.txt","w",encoding="utf-8") as outfile:
+    pprint(testOutput,outfile)
+    
+    pprint(datalog,outfile)
 
